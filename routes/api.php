0d4c555e5b290cc4b06c0e162511de4d755db1c0
Sarman-Dasa <sarmand@zignuts.com>
2023-03-29 19:29:12 +0530

    add routes for  auth,role,module,permission modules

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\auth\AuthController;
+use App\Http\Controllers\ModuleController;
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +19,59 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+/**
+ * Auth
+ */
+Route::controller(AuthController::class)->group(function () {
+    Route::post('register', 'register');
+    Route::get('/account-verify/{token}', 'verifyAccount');
+    Route::post('login', 'login');
+    Route::post('forgot-password', 'forgotPassword');
+    Route::post('reset-password', 'resetPassword');
+});
+
+/**
+ * Role 
+ */
+Route::controller(RoleController::class)->middleware(['auth:sanctum','user-permission'])->prefix('role')->group(function()
+{
+    Route::post('list', 'list');
+    Route::post('create', 'create');
+    Route::post('update/{id}', 'update');
+    Route::get('get/{id}', 'get');
+    Route::delete('delete/{id}', 'destroy');
+});
+
+/**
+ * User
+ */
+Route::middleware(['auth:sanctum','throttle:1|30'])->group(function () {
+    Route::controller(UserController::class)->group(function () {
+        Route::post('list', 'list');
+        Route::get('get/{id}', 'get');
+        Route::get('logout', 'logout');
+        Route::post('change-password', 'changePassword');
+    });
+});
+
+/**
+ * Module
+ */
+Route::controller(ModuleController::class)->prefix('module')->group(function () {
+    Route::post('list', 'list');
+    Route::post('create', 'create');
+    Route::post('update/{id}', 'update');
+    Route::get('get/{id}', 'get');
+    Route::delete('delete/{id}', 'destroy');
+});
+
+/**
+ * Permission
+ */
+Route::controller(PermissionController::class)->prefix('permission')->group(function () {
+    Route::post('list', 'list');
+    Route::post('create', 'create');
+    Route::post('update/{id}', 'update');
+    Route::get('get/{id}', 'get');
+    Route::delete('delete/{id}', 'destroy');
 });
