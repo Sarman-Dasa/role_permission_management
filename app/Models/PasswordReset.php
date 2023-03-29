85ed94f802af491b3646de6af8bc90a923d4cafa
Sarman-Dasa <sarmand@zignuts.com>
2023-03-29 19:24:00 +0530

    create model for password reset

---
+++ app/Models/PasswordReset.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PasswordReset extends Model
+{
+    use HasFactory;
+    const UPDATED_AT = null;
+   // protected $primaryKey = 'email';
+
+    protected $fillable = [
+        'email',
+        'token',
+        'expired_at',
+    ];
+}
