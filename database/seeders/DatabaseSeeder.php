467489658bf88bb2c32cae584529200aff4f7d1d
Sarman-Dasa <sarmand@zignuts.com>
2023-03-31 17:22:39 +0530

    add user data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,9 +19,16 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+       $user = User::create([
+            'first_name'        => 'Super',
+            'last_name'         => 'Admin',
+            'email'             => 'sadmin@example.com',
+            'phone'             => '1234567890',
+            'email_verified_at' =>  now(),
+            'password'          =>  Hash::make(12345678),
+            'is_active'         => true,
+        ]);
+
+        $user->roles()->attach(1);
     }
 }
