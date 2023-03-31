e58a1be5c4e7523780dac66775776125a471366b
Sarman-Dasa <sarmand@zignuts.com>
2023-03-31 17:22:52 +0530

    add default super-admin role

---
+++ database/seeders/RoleSeede.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoleSeede extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Role::create([
+            'name' => 'Super-Admin',
+            'description' => 'Access all modules',
+        ]);
+    }
+}
