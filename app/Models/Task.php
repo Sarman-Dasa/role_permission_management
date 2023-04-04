15263357e5c509a70c9383fae4910325f2e5061b
Sarman-Dasa <sarmand@zignuts.com>
2023-04-04 19:08:35 +0530

    add relationship

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title' ,'description' ,'employee_id'];
+
+    /**
+     * Task-Employee Relation
+     */
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class ,'employee_id' ,'id');
+    }
 }
