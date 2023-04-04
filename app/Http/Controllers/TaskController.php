3ead61c481ee9d502f384e6d55fd15a4b28519b7
Sarman-Dasa <sarmand@zignuts.com>
2023-04-04 19:07:36 +0530

    add crud operation

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,30 +2,84 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreTask;
+use App\Models\Task;
+use App\Traits\ListingApiTrait;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
+    use ListingApiTrait;
     /**
-     * Display a listing of the resource.
+     * Display a listing of the task.
      *
      * @return \Illuminate\Http\Response
      */
     public function list()
     {
-        return ok('Permission granted');
+        
+        $this->ListingValidation();
+
+        $query = Task::query();
+        $searchable_fileds = ['title' ,'description'];
+        
+        $data = $this->filterSearchPagination($query ,$searchable_fileds);
+
+        return ok('Task list',[
+            'tasks' =>  $data['query']->get(),
+            'count' =>  $data['count'],
+        ]);
+
+        //return ok('Permission granted');
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Store a newly created task in data.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function create(Request $request)
+    public function create(StoreTask $request)
     {
-        return ok('Permission granted');
+        // $request->validate([
+        //     'title'         =>  'required|min:3|max:100',
+        //     'description'   =>  'required|min:5|max:255',
+        //     'employee_id'   =>  'required|exists:employees,id'
+        // ]);
+
+        $task = Task::create($request->validated());
+        return ok('Task data added successfully');
+        
+        //$task = Task::create($request->only(['title' ,'description' ,'employee_id']));
+
+      
+        //return ok('Permission granted');
     }
+
+    /**
+     * Update the specified task in database.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $task = Task::findOrFail($id);
+
+        $request->validate([
+            'title'         =>  'required|min:3|max:100',
+            'description'   =>  'required|min:5|max:255',
+            'employee_id'   =>  'required|exists:employees,id'
+        ]);
+
+        $task->update($request->only(['title' ,'description' ,'employee_id']));
+
+        return ok('Task data uppdated successfully');
+        
+        //return ok('Permission granted');
+    }
+
 
     /**
      * Display the specified resource.
@@ -35,19 +89,10 @@ class TaskController extends Controller
      */
     public function get($id)
     {
-        return ok('Permission granted');
-    }
+        $task = Task::with('employee','employee.user')->findOrFail($id);
+        return ok('Task Data',$task);
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        return ok('Permission granted');
+        //return ok('Permission granted');
     }
 
     /**
@@ -58,6 +103,11 @@ class TaskController extends Controller
      */
     public function destroy($id)
     {
-        return ok('Permission granted');
+        $task = Task::findOrFail($id);
+        $task->delete();
+
+        return ok('task deleted successfully');
+        
+        //return ok('Permission granted');
     }
 }
