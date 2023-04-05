c5677170960753cccd49f3bcb71625d6b416fcdf
Sarman-Dasa <sarmand@zignuts.com>
2023-04-05 19:22:11 +0530

    add job for send mail

---
+++ app/Jobs/SendNotificationMail.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Notifications\AccountVerifyMail;
+use App\Notifications\WelcomeMail;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class SendNotificationMail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public $user;
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->user->notify(new WelcomeMail());
+        $this->user->notify(new AccountVerifyMail($this->user));
+    }
+}
