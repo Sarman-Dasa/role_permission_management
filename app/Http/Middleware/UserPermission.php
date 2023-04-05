56f0bbdf8606243783e5a0cf6282ff85dfdae481
Sarman-Dasa <sarmand@zignuts.com>
2023-04-05 12:25:52 +0530

    removed extra code

--- app/Http/Middleware/UserPermission.php
+++ app/Http/Middleware/UserPermission.php
@@ -2,12 +2,8 @@
 
 namespace App\Http\Middleware;
 
-use App\Models\Permission;
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-
-use function PHPUnit\Framework\equalTo;
 
 class UserPermission
 {
@@ -20,23 +16,7 @@ class UserPermission
      */
     public function handle(Request $request, Closure $next,$model,$access)
     {
-    
-    /*
-        dd([$model, $access]);
-        $userRole = Auth::user()->roles->pluck('name');
-        $permissions = auth()->user()->roles()->with('permissions.modules')->get(); //->pluck('permissions');
-        $permissions = auth()->user()->roles()->with('permissions.modules')->get()->pluck('permissions')->toArray();
 
-        dd($permissions);
-        if (count($permissions[0])) {
-            return $next($request);
-        }
-
-        dd($user);
-        if ($userRole[0] == "Super admin") {
-            return $next($request);
-        }
-    */
         $user = auth()->user();
         if($user->hasAccess($model,$access))
         {
