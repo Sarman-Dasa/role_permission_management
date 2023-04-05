4d7c3108355aced390c208d6a0364d103b86ca4d
Sarman-Dasa <sarmand@zignuts.com>
2023-04-05 12:22:49 +0530

    add comment & update, delete function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,6 +11,11 @@ class UserController extends Controller
 {
     use ListingApiTrait;
 
+    /**
+     * Display a listing of the user.
+     * @param \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function list(Request $request)
     {
         $this->ListingValidation();
@@ -25,31 +30,87 @@ class UserController extends Controller
         ]);
     }
 
-    public function get($id)
+    /**
+     * Update the specified user in database.
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
     {
-        $user = User::with('roles','roles.permissions')->findOrFail($id);
-        return ok('User profile',$user);
+        $user = auth()->user();
+        $id = $user->id;
+        $request->validate([
+            'first_name'            =>  'required|string|min:3|max:30',
+            'last_name'             =>  'required|string|min:3|max:30',
+            'email'                 =>  'required|email|unique:users,email,' . $id . ',id',
+            'password'              =>  'required|confirmed',
+            'phone'                 =>  'required|numeric|unique:users,phone,' . $id . ',id',
+            'role_id'               =>  'required|array|exists:roles,id',
+        ]);
+
+
+        $user->update($request->only(['first_name', 'last_name', 'email', 'phone'])
+            + [
+                'password'              =>  Hash::make($request->password),
+            ]);
+
+        $user->roles()->sync($request->role_id);
+
+        return ok('user data updated successfuly');
     }
 
+    /**
+     * Display the specified user.
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function get($id)
+    {
+        $user = User::with('roles', 'roles.permissions')->findOrFail($id);
+        return ok('User profile', $user);
+    }
+
+    /**
+     * logout the specified user.
+     * @return \Illuminate\Http\Response
+     */
     public function logout()
     {
         $user = auth()->user()->tokens();
         $user->delete();
     }
 
+    /**
+     * change the specified user password.
+     *@param \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function changePassword(Request $request)
     {
         $request->validate([
             'current_password'      =>  'required|current_password',
             'password'              =>  'required|min:8|max:12',
-            'password_confirmation' =>  'required|same:password', 
+            'password_confirmation' =>  'required|same:password',
         ]);
 
         $user = auth()->user();
         $user->update([
             'password' => Hash::make($request->password),
         ]);
-        
+
         return ok('Password changed successfully');
+    }
+
+     /**
+     * Remove the specified user from database.
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy()
+    {
+        $user = auth()->user();
+        $user->delete();
+
+        return ok("Account deleted successfully");
     }
 }
