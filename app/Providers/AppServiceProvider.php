590b4fed9a6532c9f06c4ca1b8a0743c1317f9a9
Sarman-Dasa <sarmand@zignuts.com>
2023-04-06 19:25:56 +0530

    bind a facade class

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+//use App\Facades\ItDateClass;
+
+use App\Facades\ItDateClass;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +16,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind('itdateclass',function(){
+            return new ItDateClass();
+        });
     }
 
     /**
