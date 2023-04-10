84c55157284371dcaf70df525cb13a928765f80a
Sarman-Dasa <sarmand@zignuts.com>
2023-04-10 19:16:43 +0530

    create blade file for pdf view

---
+++ resources/views/userPdf.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <table border="1">
+        <tr>
+            <td>First Name</td>
+            <td>Last Name</td>
+            <td>Email</td>
+            <td>Phone</td>
+        </tr>
+
+        @foreach ($users as $user)
+            <tr>
+                <td>{{$user->first_name}}</td>
+                <td>{{$user->last_name}}</td>
+                <td>{{$user->email}}</td>
+                <td>{{$user->phone}}</td>
+            </tr>
+        @endforeach
+    </table>
+</body>
+</html>
