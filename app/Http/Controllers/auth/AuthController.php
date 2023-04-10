64dbe84849751dbb2c8a3c7ada08339d24fac6f6
Sarman-Dasa <sarmand@zignuts.com>
2023-04-10 19:14:03 +0530

    add functionality to login with mobile number otp

--- app/Http/Controllers/auth/AuthController.php
+++ app/Http/Controllers/auth/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\auth;
 use App\Http\Controllers\Controller;
 use App\Models\PasswordReset;
 use App\Models\User;
+use App\Models\UserOtp;
 use App\Notifications\AccountVerifyMail;
 use App\Notifications\PasswordResetMail;
 use App\Notifications\WelcomeMail;
@@ -13,6 +14,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Log;
+use Twilio\Rest\Client;
 
 class AuthController extends Controller
 {
@@ -33,12 +36,12 @@ class AuthController extends Controller
             'role_id'               =>  'required|array|exists:roles,id',
         ]);
 
-        $user = User::create($request->only(['first_name' ,'last_name' ,'email' ,'phone'])
+        $user = User::create($request->only(['first_name', 'last_name', 'email', 'phone'])
             + [
                 'password'              =>  Hash::make($request->password),
                 'email_verify_token'    =>  Str::random(64),
             ]);
-        
+
         $user->roles()->attach($request->role_id);
         //$user->notify(new WelcomeMail());
         //$user->notify(new AccountVerifyMail($user));
@@ -53,21 +56,20 @@ class AuthController extends Controller
      */
     public function verifyAccount($token)
     {
- 
-         $user = User::where('email_verify_token',$token)->first();
-         if($user)
-         {
-             $user->update([
-                 'email_verify_token'    =>  null,
-                 'email_verified_at'     => Carbon::now(),
-                 'is_active'             =>  true,
-             ]);
-             return ok('Account Verify Successfuly');
-         }else{
-             return ok('Account Already Verified');
-         }
+
+        $user = User::where('email_verify_token', $token)->first();
+        if ($user) {
+            $user->update([
+                'email_verify_token'    =>  null,
+                'email_verified_at'     => Carbon::now(),
+                'is_active'             =>  true,
+            ]);
+            return ok('Account Verify Successfuly');
+        } else {
+            return ok('Account Already Verified');
+        }
     }
-    
+
     /**
      * user login 
      * @param \Illuminate\Http\Request  $request
@@ -75,22 +77,21 @@ class AuthController extends Controller
      */
     public function login(Request $request)
     {
- 
-         $request->validate([
-             'email'    =>  'required|email|exists:users,email',
-             'password' =>  'required',
-         ]);
- 
-         if(Auth::attempt(['email' => $request->email, 'password' => $request->password , 'is_active' => true]))
-         {
-             $user   =   auth()->user();
-             $token  =  $user->createToken('API TOKEN')->plainTextToken;
-             return ok('User Login Successfully',$token);
-         }
-         return ok('Invalid Email & Password');
+
+        $request->validate([
+            'email'    =>  'required|email|exists:users,email',
+            'password' =>  'required',
+        ]);
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password, 'is_active' => true])) {
+            $user   =   auth()->user();
+            $token  =  $user->createToken('API TOKEN')->plainTextToken;
+            return ok('User Login Successfully', $token);
+        }
+        return ok('Invalid Email & Password');
     }
 
-    
+
     /**
      * @func forgotPassword
      * @param Request $request 
@@ -141,12 +142,84 @@ class AuthController extends Controller
                 ['email' => $passwordReset->email],
                 [
                     'password' => Hash::make($request->password),
-                ]);
-            
-                $passwordReset->where('email',$passwordReset->email)->delete();
-            
+                ]
+            );
+
+            $passwordReset->where('email', $passwordReset->email)->delete();
+
             return ok('Password Reset Successfully');
         }
-        return error('Password Reset Token Expired!!!',type:'unauthenticated');
+        return error('Password Reset Token Expired!!!', type: 'unauthenticated');
+    }
+
+    /**
+     * @func sentOtp
+     * @description 'send otp of given number'
+     * @param Request $request
+     * @return json response
+     */
+    public function sendOtp(Request $request)
+    {
+        $request->validate([
+            'phone' =>  'required|numeric|digits:10|exists:users,phone'
+        ]);
+
+        return $this->genrateOtp($request->phone);
+    }
+
+    /**
+     * @func sentOtp
+     * @description 'send otp of given number'
+     * @param phoneNumber 
+     * @return json response
+     */
+    public function genrateOtp($phone)
+    {
+        $otp = rand(0000, 9999);
+
+        $userOtp = UserOtp::updateOrCreate(
+            ['phone'    =>  $phone],
+            [
+                'otp'           =>  $otp,
+                'expired_at'    =>  now()->addMinutes(3),
+            ]
+        );
+
+        $account_sid = getenv("TWILIO_SID");
+        $auth_token = getenv("TWILIO_TOKEN");
+        $twilio_number = getenv("TWILIO_FROM");
+
+        $client = new Client($account_sid, $auth_token);
+        $client->messages->create("+91 " . $phone, [
+            'from'  =>  $twilio_number,
+            'body'  =>  $otp,
+        ]);
+        Log::channel('customelog')->info('Otp :- ' . $otp);
+        return ok("otp send Successfully");
+    }
+
+    /**
+     * @func verifyOtp
+     * @description 'verify otp then user login'
+     * @param Request $request
+     * @return json response
+     */
+    public function verifyOtp(Request $request)
+    {
+        $request->validate([
+            'otp'   =>  'required|numeric|exists:user_otps,otp'
+        ]);
+
+        $userOtp = UserOtp::where('otp', $request->otp)->first();
+        $user  = User::where('phone', $userOtp->phone)->first();
+        if ($userOtp->expired_at >= now()) {
+            $token = $user->createToken('API TOKEN')->plainTextToken;
+            Auth::login($user, true);
+            $userOtp->delete();
+            return ok("Login successfully", [
+                'token' =>  $token,
+            ]);
+        }
+        return $this->genrateOtp($user->phone);
     }
 }
